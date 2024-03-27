2bc1515b968f8e1dcdfb496031faedd3175a5deb
jf.webermarx <jf.webermarx@criteo.com>
2024-03-27 22:20:43 +0000

    First test of api
    Signed-off-by: jf.webermarx <jf.webermarx@criteo.com>

---
+++ api/api.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from mlcc.engine.engine import Engine
+
+app = FastAPI()
+engine = Engine()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/foods/{food_id}")
+def read_food(food_id: str, q: Union[str, None] = None):
+    return {"food_id": food_id, "q": q, "food": engine.food_data.select_food(food_id)}
