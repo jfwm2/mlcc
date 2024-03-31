0494ef8db43059df4a71097ce682f4d60519ee8d
jf.webermarx <jf.webermarx@criteo.com>
2024-03-31 14:13:37 +0000

    Add set_current_food to hybrid client
    Signed-off-by: jf.webermarx <jf.webermarx@criteo.com>

--- mlcc/clients/client_implementations/hybrid_client_implementation.py
+++ mlcc/clients/client_implementations/hybrid_client_implementation.py
@@ -1,7 +1,11 @@
+from typing import List, Optional
+
 import requests
 
 from mlcc.clients.client_implementations.abstract_client_implementation import AbstractClientImplementation
+from mlcc.clients.text_input import input_string_with_trie
 from mlcc.common.defaults import DEFAULT_API_URL
+from mlcc.common.trie import Trie
 
 
 class HybridClientImplementation(AbstractClientImplementation):
@@ -11,18 +15,27 @@ class HybridClientImplementation(AbstractClientImplementation):
         self.api_url = api_url
 
     def display_food_data(self) -> None:
-        response = requests.get(f"{self.api_url}/foods")
-        foods_response = response.json().get('foods', None)
-        if foods_response is None:
-            print("<food list could not be retrieved>")
-            return None
-        for name in foods_response:
-            response = requests.get(f"{self.api_url}/foods/{name}")
-            food_response = response.json().get('food', None)
-            description = '<food description could not be retrieved>' \
-                if food_response is None or 'description' not in food_response else food_response['description']
-            print(f"{name}: {description}")
+        food_names = self._get_food_names()
+        if food_names is not None:
+            for name in food_names:
+                response = requests.get(f"{self.api_url}/foods/{name}")
+                food_response = response.json().get('food', None)
+                description = '<food description could not be retrieved>' \
+                    if food_response is None or 'description' not in food_response else food_response['description']
+                print(f"{name}: {description}")
+
+    def set_current_food(self) -> None:
+        food_names = self._get_food_names()
+        if food_names is not None:
+            self.current_food_name = input_string_with_trie("Name of the food to select", Trie(food_names))
 
     @staticmethod
     def exit() -> None:
         print("Exiting Hybrid Client")
+
+    def _get_food_names(self) -> Optional[List[str]]:
+        response = requests.get(f"{self.api_url}/foods")
+        foods_response = response.json().get('foods', None)
+        if foods_response is None:
+            print("<food list could not be retrieved>")
+        return foods_response
